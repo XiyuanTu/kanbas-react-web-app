f22add12692a854dafd58660400a399c9392affd
Xiyuan Tu <xiyuan.tyler@gmail.com>
2024-09-19 20:55:24 -0700

    small fix

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -2,23 +2,41 @@ import { Link } from "react-router-dom";
 export default function CoursesNavigation() {
   return (
     <div id="wd-courses-navigation">
-      <Link rel="noreferrer" to="/Kanbas/Courses/1234/Home">
+      <Link target="_blank" rel="noreferrer" to="/Kanbas/Courses/1234/Home">
         Home
       </Link>
       <br />
-      <Link to="/Kanbas/Courses/1234/Modules">Modules</Link>
+      <Link target="_blank" rel="noreferrer" to="/Kanbas/Courses/1234/Modules">
+        Modules
+      </Link>
       <br />
-      <Link to="/Kanbas/Courses/1234/Piazza">Piazza</Link>
+      <Link target="_blank" rel="noreferrer" to="/Kanbas/Courses/1234/Piazza">
+        Piazza
+      </Link>
       <br />
-      <Link to="/Kanbas/Courses/1234/Zoom">Zoom</Link>
+      <Link target="_blank" rel="noreferrer" to="/Kanbas/Courses/1234/Zoom">
+        Zoom
+      </Link>
       <br />
-      <Link to="/Kanbas/Courses/1234/Assignments">Assignments</Link>
+      <Link
+        target="_blank"
+        rel="noreferrer"
+        to="/Kanbas/Courses/1234/Assignments"
+      >
+        Assignments
+      </Link>
       <br />
-      <Link to="/Kanbas/Courses/1234/Quizzes">Quizzes</Link>
+      <Link target="_blank" rel="noreferrer" to="/Kanbas/Courses/1234/Quizzes">
+        Quizzes
+      </Link>
       <br />
-      <Link to="/Kanbas/Courses/1234/Grades">Grades</Link>
+      <Link target="_blank" rel="noreferrer" to="/Kanbas/Courses/1234/Grades">
+        Grades
+      </Link>
       <br />
-      <Link to="/Kanbas/Courses/1234/People">People</Link>
+      <Link target="_blank" rel="noreferrer" to="/Kanbas/Courses/1234/People">
+        People
+      </Link>
       <br />
     </div>
   );
